9c35009b1393db678e41ed16159f97a82ded140a
Mortynex <Mortynex@users.noreply.github.com>
2020-06-21 19:49:17 +0200

    Update -1.98

---
+++ 0/np/script.js
@@ -0,0 +1,58 @@
+
+let hdvisble;
+let hd;
+window.onload = function(){
+    
+}
+window.addEventListener("load",()=>{
+    let textbox = document.getElementById("editor");
+    hd = document.getElementById("hd");
+    let text = localStorage.npcontent;
+    hdvisble = localStorage.getItem('nphdvisible');
+    if(!hdvisble){
+        console.log("Accountedssssssaew");
+        localStorage.nphdvisible = false;
+        hdvisble = false;
+    }
+    if(text){
+        console.log("Content Loaded");
+        textbox.value = text;
+    }
+    else{
+        console.log("Nothing Saved");
+    }
+    if(hdvisble =="true"){
+        console.log("Accountedsss");
+        hd.style.display = "block";
+    }
+    else if(hdvisble=="false"){
+        console.log("Accounted");
+        hd.style.display = "none";
+    }
+    if(localStorage.blackmode == "true"){
+        textbox.classList.add("inverted");
+    }
+    textbox.addEventListener("keyup",()=>{
+        localStorage.npcontent = textbox.value;
+        text = textbox.value;
+    });
+});
+function toggleHeader(){
+    if(hdvisble){
+        localStorage.nphdvisible = false;
+        hdvisble = false;
+        hd.style.display = "none";
+    }
+    else{
+        localStorage.nphdvisible = true;
+        hdvisble = true;
+        hd.style.display = "block";
+    }
+}
+document.addEventListener("keyup",event => {
+    console.log("Toggled");
+    if(event.key == "Escape"){
+        toggleHeader();
+        console.log("Toggled");
+    }
+});
---
+++ 0/sw/script.js
@@ -0,0 +1,63 @@
+let sec = 0;
+let min = 0;
+let ms = 0;
+
+let running = false;
+let timer;
+console.log('connected');
+function update() {
+	ms++;
+
+	if (ms == 100) {
+		sec++;
+		ms = 0;
+	}
+	if (sec == 60) {
+		min++;
+		sec = 0;
+	}
+	if (min == 60) {
+		min == 0;
+	}
+    //00 : 00 . 00
+    
+	document.getElementById('swmin').innerHTML = addZero(min);
+	document.getElementById('swsec').innerHTML = addZero(sec);
+	document.getElementById('swms').innerHTML = addZero(ms);
+}
+document.addEventListener('keydown',event => {
+    if(event.code == "Space"){
+        toggle();
+    }
+    /*if(event.code == "Escape"){
+        cleared();
+    }
+    if(event.code == "Backspace"){
+        cleared();
+    }*/
+});
+function addZero(number) {
+	if (number < 10) {
+		return '0' + number;
+	} else {
+		return number;
+	}
+}
+function toggle() {
+	if (!running) {
+		running = true;
+		timer = setInterval(update, 10);
+		document.getElementById('tg').innerHTML = "Stop ";
+	} else {
+		running = false;
+		document.getElementById('tg').innerHTML = "Start";
+		clearInterval(timer);
+	}
+}
+//document.getElementById("clear").addEventListener("click", cleared)
+function cleared() {
+	sec = min = ms = 0;
+	document.getElementById('swmin').innerHTML = addZero(min);
+	document.getElementById('swsec').innerHTML = addZero(sec);
+	document.getElementById('swms').innerHTML = addZero(ms);
+}
